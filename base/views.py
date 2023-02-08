a0772dc2ad118f78c428df3e7269d224f77f2274
PHẠM VĂN DUY <80450303+P-Duy@users.noreply.github.com>
2023-02-08 16:12:02 +0700

    update

--- base/views.py
+++ base/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms= [
-    {'id':1, 'name':'Lest learn python'},
-    {'id':2, 'name':'Design wit me'},
-    {'id':3, 'name':'Frontend developer'},
-]
+# rooms= [
+#     {'id':1, 'name':'Lest learn python'},
+#     {'id':2, 'name':'Design wit me'},
+#     {'id':3, 'name':'Frontend developer'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
 
+def createRoom(request):
+    
+    context = {}
+    return render(request, 'base/room_from.html', context)
