53ad40d483efe8ed7687a0249a18729e868ff816
PHẠM VĂN DUY <80450303+P-Duy@users.noreply.github.com>
2023-02-13 15:49:23 +0700

    message

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout #trả về phương thức xác thực đăng nhập xuất.
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .models import Room ,Topic
+from .models import Room ,Topic, Message
 from .forms import RoomForm
 
 # rooms= [
@@ -78,12 +78,23 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    messages = room.message_set.all()
-    context = {'room':room, 'messages':messages}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user,)
+        return redirect('room', pk=room.id)
+    
+    context = {'room':room, 'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html', context)
 
 
-@login_required(login_url='/login')
+@login_required(login_url='/login') 
 def createRoom(request):
     form = RoomForm()
     
@@ -126,3 +137,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html',{'obj':room})
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse(' You are not allowed to here')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':message})
