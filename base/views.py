f620ebbc36239c4b4d8b57051ca6c8832518bc60
PHẠM VĂN DUY <80450303+P-Duy@users.noreply.github.com>
2023-02-15 13:44:46 +0700

    static file

--- base/views.py
+++ base/views.py
@@ -70,15 +70,17 @@ def home(request):
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
-        Q(description__icontains=q))
+        Q(description__icontains=q)
+        )
     room_count = rooms.count()
-    context = {'rooms':rooms, 'topic':topic, 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms':rooms, 'topic':topic, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     
     if request.method == 'POST':
@@ -94,6 +96,15 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages= user.message_set.all()
+    topic =Topic.objects.all()
+    context ={'user':user, 'rooms':rooms,'room_messages':room_messages,'topic':topic}
+    return render(request,'base/profile.html', context)
+
+
 @login_required(login_url='/login') 
 def createRoom(request):
     form = RoomForm()
@@ -101,7 +112,9 @@ def createRoom(request):
     if request.method=='POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room=form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
         
     context = {'form':form}
